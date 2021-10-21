e0f01346494490165ccf67a3a6e2e1617e8b83c5
Hatice Sönmez <htc.sonmez@hotmail.com>
2021-10-21 20:41:32 +0300

    no msg

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
 console.log("Hello World")
+
+let krediler = ["Konut Kredisi", "Taşıt Kredisi", "İhtiyaç Kredisi"]
+
+for (let index = 0; index < krediler.length; index++) {
+    console.log(krediler[index])
+    
+}
