adee85a9d2fdacaf40480a25769f15b9dbccf0bb
Hatice Sönmez <htc.sonmez@hotmail.com>
2021-10-22 21:46:06 +0300

    homework done

--- src/restSpreadDestructure/restSpreadDestructure.js
+++ src/restSpreadDestructure/restSpreadDestructure.js
@@ -1,24 +1,3 @@
-console.log("Hello World")
-
-let krediler = ["Konut Kredisi", "Taşıt Kredisi", "İhtiyaç Kredisi"]
-
-for (let index = 0; index < krediler.length; index++) {
-    console.log(krediler[index])  
-}
-
-let student = {id:1, name: "Hatice"}
-
-
-function save(puan = 10, student) {
-    console.log(student.name + " : " + puan)
-}
-
-save(undefined, student)
-
-let students = [student, {id:2, name: "Abc"}]
-console.log(students)
-
-
 //rest
 let showProducts = function name(id, ...products) {
     console.log(id)
